c3b1bd55fba14d319af3b5ca623f526de36e47e8
maruk0chan <marco.chan@penqe.com>
2023-03-11 05:18:54 +0900

    use Typescript in index page and add input el

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import ResultTable from '../ResultTable.svelte'
+	import testData from '../json/test.json'
+
+	let name = ''
+
+	const handleInput = (event: Event) => {
+		const target = event.target as HTMLInputElement
+		name = target.value
+	}
+</script>
+
+<h1>Welcome to Gourmet in Japan</h1>
+<p>Select some conditions to filter out the restaurants you are looking for 😉</p>
+<input type="text" on:input={handleInput} />
+<ResultTable />
+
+<style>
+</style>
