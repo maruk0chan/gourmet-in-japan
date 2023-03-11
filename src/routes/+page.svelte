afa9ea02c50e1895a60c7ab99f82990956a8ab91
maruk0chan <marco.chan@penqe.com>
2023-03-11 19:19:20 +0900

    create table component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -98,41 +98,7 @@
 		</div>
 	</div>
 </div>
-<div class="result-container">
-	<div class="result-stat">
-		{#if gourmetData}
-			<div>Total no. of results: {gourmetData.results.results_available}</div>
-			<div>No. of results shown below: {gourmetData.results.results_returned}</div>
-		{:else}
-			<div>Total no. of results: 0</div>
-			<div>No. of results shown below: 0</div>
-		{/if}
-	</div>
-	<table>
-		<thead>
-			<tr>
-				{#each columns as column}
-					<th>{column}</th>
-				{/each}
-			</tr>
-		</thead>
-		{#if shopList}
-			<tbody>
-				{#each shopList as shop}
-					<tr>
-						{#each columns as column}
-							{#if ['genre', 'budget'].includes(column)}
-								<th>{shop[column].name}</th>
-							{:else}
-								<th>{shop[column]}</th>
-							{/if}
-						{/each}
-					</tr>
-				{/each}
-			</tbody>
-		{/if}
-	</table>
-</div>
+<ResultTable {gourmetData} {columns} {shopList} />
 
 <style lang="sass">
 	.heading
@@ -157,7 +123,4 @@
 		margin-top: 20px
 	.search-button
 		margin-top: 20px
-	.result-container
-		padding: 0 50px
-		max-width: initial
 </style>
