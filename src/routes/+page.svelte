07ce8ec982d524e5e1a0ce022dde5719b0a87974
maruk0chan <marco.chan@penqe.com>
2023-03-11 19:09:32 +0900

    adjust html structure and style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 	let selectedCount
 	let shopList
 	const columns = ['station_name', 'genre', 'budget', 'open', 'close', 'capacity']
-	
+
 	async function makeQuery() {
 		const maxSelections = 5
 		const smallAreaParams = () => {
@@ -61,41 +61,44 @@
 </script>
 
 <div class="search-container">
-	<h1>Welcome to Gourmet in Japan</h1>
-	<p>Select some conditions to filter out the restaurants you are looking for 😉</p>
+	<h1 class="heading">Welcome to Gourmet in Japan</h1>
+	<p>This is an web application to help you find the restaurants that meet your search condition. 😉</p>
+	<p>Select some conditions to filter out the restaurants you are looking for</p>
 	<div>For multiple selections, <b><u>ONLY the FIRST 5</u></b> selections will be applied and you can also <b><u>DRAG</u></b> to change the orders</div>
-	<div class="condition">
-		<label for="area">Choose areas</label>
-		<AutoComplete
-			multiple="true"
-			delay="500"
-			orderableSelection="true"
-			items={areaList}
-			bind:selectedItem={selectedAreaObject}
-			bind:value={selectedAreaId}
-			labelFieldName="ja"
-			valueFieldName="id"
-			labelFunction={(area) => area.ja + ' ' + area.en}
-			keywordsFunction={(area) => area.ja + ' ' + area.en + ' ' + area.hg + ' ' + area.kk}
-		/>
+	<div class="filter-container">
+		<div class="filter">
+			<label for="area">Choose areas</label>
+			<AutoComplete
+				multiple="true"
+				delay="500"
+				orderableSelection="true"
+				items={areaList}
+				bind:selectedItem={selectedAreaObject}
+				bind:value={selectedAreaId}
+				labelFieldName="ja"
+				valueFieldName="id"
+				labelFunction={(area) => area.ja + ' ' + area.en}
+				keywordsFunction={(area) => area.ja + ' ' + area.en + ' ' + area.hg + ' ' + area.kk}
+			/>
+		</div>
+		<div class="filter">
+			<label for="budget">Budget (¥)</label>
+			<AutoComplete multiple="true" readonly={true} items={budgetOptions} bind:selectedItem={selectedBudgetObject} bind:value={selectedBudgetId} labelFieldName="name" valueFieldName="code" />
+		</div>
+		<div class="filter">
+			<label for="count">Results per page</label>
+			<select id="count-select" on:change={handleCountSelect}>
+				{#each countOptions as countOption}
+					<option value={countOption}>{countOption}</option>
+				{/each}
+			</select>
+		</div>
+		<div class="submit-panel">
+			<button class="search-button" on:click={makeQuery}>Search</button>
+		</div>
 	</div>
-	<div class="condition">
-		<label for="budget">Budget (¥)</label>
-
-		<AutoComplete multiple="true" readonly={true} items={budgetOptions} bind:selectedItem={selectedBudgetObject} bind:value={selectedBudgetId} labelFieldName="name" valueFieldName="code" />
-	</div>
-	<div class="condition">
-		<label for="count">Results per page</label>
-		<select id="count-select" on:change={handleCountSelect}>
-			{#each countOptions as countOption}
-				<option value={countOption}>{countOption}</option>
-			{/each}
-		</select>
-	</div>
-	<div class="submit-panel">
-		<button class="search-button" on:click={makeQuery}>Search</button>
-	</div>
-
+</div>
+<div class="result-container">
 	<div class="result-stat">
 		{#if gourmetData}
 			<div>Total no. of results: {gourmetData.results.results_available}</div>
@@ -105,47 +108,56 @@
 			<div>No. of results shown below: 0</div>
 		{/if}
 	</div>
-
-	<div class="result-container">
-		<table>
-			<thead>
-				<tr>
-					{#each columns as column}
-						<th>{column}</th>
-					{/each}
-				</tr>
-			</thead>
-			{#if shopList}
-				<tbody>
-					{#each shopList as shop}
-						<tr>
-							{#each columns as column}
+	<table>
+		<thead>
+			<tr>
+				{#each columns as column}
+					<th>{column}</th>
+				{/each}
+			</tr>
+		</thead>
+		{#if shopList}
+			<tbody>
+				{#each shopList as shop}
+					<tr>
+						{#each columns as column}
+							{#if ['genre', 'budget'].includes(column)}
+								<th>{shop[column].name}</th>
+							{:else}
 								<th>{shop[column]}</th>
-							{/each}
-						</tr>
-					{/each}
-				</tbody>
-			{/if}
-		</table>
-	</div>
+							{/if}
+						{/each}
+					</tr>
+				{/each}
+			</tbody>
+		{/if}
+	</table>
 </div>
 
 <style lang="sass">
-.search-container
-	max-width: 1000px
-	margin: 0 auto
-.condition
-	display: flex
-	flex-direction: column
-	margin: 0 auto
-	max-width: 600px
-.condition + .condition 
-	margin-top: 20px
-.submit-panel
-	display: flex
-	justify-content: center
-.result-stat
-	margin-top: 20px
-.search-button
-	margin-top: 20px
+	.heading
+		text-align: center
+	.search-container
+		max-width: 1000px
+		padding: 0 50px
+		margin: 0 auto
+	.filter-container
+		margin-top: 50px
+		.filter
+			display: flex
+			flex-direction: column
+			margin: 0 auto
+			max-width: 600px
+		.filter + .filter 
+			margin-top: 20px
+	.submit-panel
+		display: flex
+		justify-content: center
+	.result-stat
+		margin-top: 20px
+	.search-button
+		margin-top: 20px
+	.result-container
+		padding: 0 50px
+		max-width: initial
 </style>
