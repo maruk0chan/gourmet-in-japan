c36ce883750b7f904f48ed327e81216c14c90f10
maruk0chan <marco.chan@penqe.com>
2023-03-12 00:23:14 +0900

    add adapter config esp. the fallback key

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-const dev = process.argv.includes('dev');
+const dev = process.argv.includes('dev')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +12,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ strict: false }),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: false,
+		}),
 		appDir: 'app',
 		paths: { base: dev ? '' : '/gourmet-in-japan' },
 		serviceWorker: { register: false },
